99035578ef3295c8b30e56ba07e9d68be1007c38
LottieWang <wanglt@shanghaitech.edu.cn>
2023-08-16 13:12:41 -0700

    scripts

--- examples/run_example.py
+++ examples/run_example.py
@@ -2,11 +2,19 @@ import subprocess
 import os
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-subprocess.call(f'mkdir {CURRENT_DIR}/../log', shell=True)
+os.makedirs(f"{CURRENT_DIR}/../log", exist_ok=True)  
 
-GRAPH_DIR="/ssd0/graphs/link"
-graphs = ["Epinions1", "Slashdot", "DBLP", "Youtube"]
-for g in graphs:
-    out_file = f"{CURRENT_DIR}/../log/{g}_lb.out"
-    in_file = f"{GRAPH_DIR}/{g}_sym.bin"
-    cmd = f"{CURRENT_DIR}/2hop_cover {in_file} | tee {out_file}"
+GRAPH_DIR="~/data"
+graphs = ["Epinions1", "Slashdot", "DBLP"]
+
+# def collect_data(file, keywords):
+
+
+if __name__=="__main__":
+    numKbitParallel=1024
+    out_file = f"{CURRENT_DIR}/../log/stats_{numKbitParallel}.out"
+    for g in graphs:
+        in_file = f"{GRAPH_DIR}/{g}_sym.bin"
+        cmd = f"{CURRENT_DIR}/../build/examples/BFS_labeling {in_file} | tee -a {out_file}"
+        subprocess.call(cmd, shell=True)
+    
